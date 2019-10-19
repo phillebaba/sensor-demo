c96f9bca5fbb19e9264a1791cc50f89c0da7add7
Philip Laine <philip.laine@gmail.com>
2019-10-19 23:16:37 +0200

    Fix API environment variable passing at runtime
    Add timestamp indicating when that last message was received

--- web/docker/cmd.sh
+++ web/docker/cmd.sh
@@ -2,7 +2,7 @@ if [ ! -z "$API" ]
 then
   INDEX_PATH="/usr/share/nginx/html/index.html"
   INDEX_TMP_PATH="${INDEX_PATH}.tmp"
-  sed 's/<script>.*<\/script>/<script>env = { API: '$API' }<\/script>/g' $INDEX_PATH > $INDEX_TMP_PATH
+  sed "s,http://localhost:7777,$API,g" $INDEX_PATH > $INDEX_TMP_PATH
   mv $INDEX_TMP_PATH $INDEX_PATH
 fi
 
--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,10 @@
+var dateFormat = require('dateformat');
 const {grpc} = require("@improbable-eng/grpc-web");
 const {Empty} = require("google-protobuf/google/protobuf/empty_pb")
 
 const {TemperatureServiceClient} = require('../proto/temperature_pb_service.js')
 
 const client = new TemperatureServiceClient(window.env.API);
-
 const empty = new Empty()
 
 response = client.getTemperature(empty)
@@ -13,6 +13,9 @@ response.on("data", function(message) {
   hue = 180 - (180 * ((temperature + 20) / 70))
   document.getElementById("temperature").innerHTML = temperature + " °C";
   document.getElementById("box").style.background = "hsl(" + hue + ", 100%, 50%)";
+
+  updated = dateFormat("mmm dS, yyyy @ HH:MM:ss")
+  document.getElementById("last-updated").innerHTML = updated;
 }).on("status", function(data) {
   console.log(data)
 }).on("end", function(data) {
